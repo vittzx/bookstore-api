b2b6d21a9ffd7d4c6a46ab40461affde811f80ed
Vitor Cosso <vitorcosso@gmail.com>
2023-12-05 15:53:41 -0300

    refactor:  removing some imports

--- src/main/java/com/bookstoreVitor/bookstorevitorcosso/infra/TestConfig.java
+++ src/main/java/com/bookstoreVitor/bookstorevitorcosso/infra/TestConfig.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.bookstoreVitor.bookstorevitorcosso.services.DBService;
 
 @Configuration
