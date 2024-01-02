26ecb405888050bdf90e9261fd0e583c392b8e52
Vitor Cosso <vitorcosso@gmail.com>
2024-01-02 17:48:24 -0300

    fix: Cross Origin

--- src/main/java/com/bookstoreVitor/bookstorevitorcosso/controllers/CategoryController.java
+++ src/main/java/com/bookstoreVitor/bookstorevitorcosso/controllers/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CategoryController {
 
 
