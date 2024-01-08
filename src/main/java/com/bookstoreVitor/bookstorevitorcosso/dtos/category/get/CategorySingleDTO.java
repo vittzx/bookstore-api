f78376c9dd4e5df7af13228953f6ab3073c47fbe
Vitor Cosso <vitorcosso@gmail.com>
2024-01-08 14:14:38 -0300

    refactor: changing Category Snigle DTO: BooksDetaisDTO instead BookGeneralDTO

--- src/main/java/com/bookstoreVitor/bookstorevitorcosso/dtos/category/get/CategorySingleDTO.java
+++ src/main/java/com/bookstoreVitor/bookstorevitorcosso/dtos/category/get/CategorySingleDTO.java
@@ -3,12 +3,13 @@ package com.bookstoreVitor.bookstorevitorcosso.dtos.category.get;
 import java.util.List;
 
 import com.bookstoreVitor.bookstorevitorcosso.domain.Category;
-import com.bookstoreVitor.bookstorevitorcosso.dtos.book.get.BookGeneralDTO;
+import com.bookstoreVitor.bookstorevitorcosso.dtos.book.get.BookDetailsDTO;
 
-public record CategorySingleDTO(Long id, String name, String description, List<BookGeneralDTO> books) {
+
+public record CategorySingleDTO(Long id, String name, String description, List<BookDetailsDTO> books) {
 
     public CategorySingleDTO(Category category){
-        this(category.getId(), category.getName(), category.getDescription(), category.getBooks().stream().map(BookGeneralDTO::new).toList());
+        this(category.getId(), category.getName(), category.getDescription(), category.getBooks().stream().map(BookDetailsDTO::new).toList());
     }
     
 }
